c4bf3ccad837f103e771d514cb2bf2e70079cdc4
JamsterMan <ben.brad.h@gmail.com>
2025-01-19 17:24:01 -0500

    added random starting positions for targets in aim game

--- gamejam-ps16/Scripts/mini_game_aim_game.gd
+++ gamejam-ps16/Scripts/mini_game_aim_game.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+var person_count : int = 4
+
 func _minigame_timer_timeout():
 	print("Mini game end")
-	if(get_child(1).has_overlapping_areas() ):
+	if(get_child(0).get_child(0).has_overlapping_areas() ):
 		print("Mini game win")
 		get_parent()._minigame_win()
 	else:
@@ -11,9 +13,9 @@ func _minigame_timer_timeout():
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	for n in person_count:
+		var x = rng.randf_range(-40,40)
+		var y = rng.randf_range(-25,25)
+		get_child(n).global_position = Vector2.UP*y*10 + Vector2.RIGHT*x*10
